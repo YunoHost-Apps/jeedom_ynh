45f32ba37a0310b9497347633b06b5e91aae5c9e
yalh76 <yalh@yahoo.com>
2020-06-25 00:43:06 +0200

    Fix #10

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,7 +15,7 @@ apt-transport-https \
 xvfb cutycapt xauth \
 duplicity \
 at \
-php-curl php-gd php-imap php-xml php-opcache php-soap php-xmlrpc php-common php-dev php-zip php-ssh2 php-mbstring php-ldap \
+php-curl php-gd php-imap php-xml php-soap php-xmlrpc php-common php-dev php-zip php-ssh2 php-mbstring php-ldap \
 ffmpeg"
 # ntp usb-modeswitch python-serial
 extra_pkg_dependencies="libsox-fmt-mp3 sox libttspico-utils \
