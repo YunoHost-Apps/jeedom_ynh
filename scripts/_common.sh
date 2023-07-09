ec01ff6df452444d2e5bfa8964685a963285613d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-07-09 21:12:46 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.4"
+
 # dependencies used by the app
 pkg_dependencies="ntp ca-certificates unzip curl cron \
 locate tar telnet wget logrotate fail2ban dos2unix ntpdate htop iotop vim iftop smbclient \
@@ -15,7 +17,7 @@ apt-transport-https \
 xvfb cutycapt xauth \
 duplicity \
 at \
-php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-imap php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-soap php$YNH_DEFAULT_PHP_VERSION-xmlrpc php$YNH_DEFAULT_PHP_VERSION-common php$YNH_DEFAULT_PHP_VERSION-dev php$YNH_DEFAULT_PHP_VERSION-zip php$YNH_DEFAULT_PHP_VERSION-ssh2 php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-ldap \
+php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-imap php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-soap php$YNH_PHP_VERSION-xmlrpc php$YNH_PHP_VERSION-dev php$YNH_PHP_VERSION-zip php$YNH_PHP_VERSION-ssh2 php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-ldap \
 ffmpeg"
 
 # ntp usb-modeswitch python-serial
